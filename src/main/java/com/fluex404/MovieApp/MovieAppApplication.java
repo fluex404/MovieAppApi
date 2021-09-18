f979548b12eefd1306f9290e2bdd6dbeebb734bb
fluex404 <isaabqari@gmail.com>
2021-09-18 14:28:14 +0700

    first commit

---
+++ src/main/java/com/fluex404/MovieApp/MovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.fluex404.MovieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluex404/MovieApp/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluex404.MovieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
